49acf2d3db0b7e6f5f2634e76af4e9f97540104d
AZIZ <azeznsasser2004@GMAIL.COM>
2022-09-21 13:11:38 +0200

    speelhal_dag

---
+++ opleiding/leren programmeren/module 02 omgaan met/deel 01/Speelhal-dag.py
@@ -0,0 +1,8 @@
+aantal_personnen = 4
+gameseat_kost_voor_een_person_per_5min = 0.37
+gameseat_kost_voor_een_person_per_1min = 0.37 / 5
+tijd_spelen = 45
+totaal_prijs = (gameseat_kost_voor_een_person_per_1min *  aantal_personnen * tijd_spelen) 
+print ("totaal prijs" ,totaal_prijs, "euro")
+
+
