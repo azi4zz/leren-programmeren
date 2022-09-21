3e112ecf42e9ca9642b8f7982d918e1a42a1f601
AZIZ <azeznsasser2004@GMAIL.COM>
2022-09-21 12:09:39 +0200

    feestlunch

---
+++ opleiding/leren programmeren/module 02 omgaan met/deel 01/Feestlunch.py
@@ -0,0 +1,9 @@
+aantal_croissantjes = 17
+croissantjes_prijs = 0.39
+aantal_stokbroden = 2
+stokbroden_prijs = 2.78
+aantal_kortingsbonnen = 3
+kortingsbonnen = 0.50
+
+prijs =(aantal_croissantjes * croissantjes_prijs + aantal_stokbroden * stokbroden_prijs - kortingsbonnen * aantal_kortingsbonnen) 
+print ("de feestlunch prijs =",prijs, "euro")
