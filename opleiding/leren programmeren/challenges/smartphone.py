1e13ec6749663a24e5d0883b568d07d8d8ff69d2
AZIZ <azeznsasser2004@GMAIL.COM>
2022-09-22 15:28:21 +0200

    persoon

--- opleiding/leren programmeren/challenges/smartphone.py
+++ opleiding/leren programmeren/challenges/smartphone.py
@@ -1,8 +1,10 @@
-vraag1 = int(input("hoe duur is  de iphone ?"))
-vraag2 = int(input("hoe duur is  de galaxy ?"))
-verschil = vraag1 - vraag2
-if verschil<50:
-    print("de iphone is het duurst, de telefoon kost:",vraag1 )
+iphone_prijs = int(input("hoe duur is  de iphone ?"))
+galaxy_prijs = int(input("hoe duur is  de galaxy ?"))
+verschil = iphone_prijs - galaxy_prijs
+if galaxy_prijs> iphone_prijs:
+    print("het advies is dus de iphone te kopen want galaxy is duruder")
+if  verschil>50:
+    print("de iphone is het duurst, de telefoon kost:",iphone_prijs )
     print("de galaxy is goedkoopst,de telefoon kost:" ,vraag2)
     print("het advies is dus de iphone te kopen .deze is namelijk" ,verschil, "duurder dan de galaxy.")
 else:
---
+++ opleiding/leren programmeren/module 02 omgaan met/deel 02/person.py
@@ -0,0 +1,13 @@
+from tkinter import N
+
+
+n = str(input("hoe heet u?"))
+a = str(input("wat is uw adress?"))
+po = str(input("wat is uw postcode?"))
+pl = str(input("wat is uw woonnplaats?"))
+print("----------------------------------------------------")
+print("naam:"  + n)
+print("Adress:"  + a)
+print("postcode:"  + po)
+print("woonplaats:" + pl)
+print("----------------------------------------------------")
