06947eb689b1b9831735ee849d2dbd151271f9ff
AZIZ <azeznsasser2004@GMAIL.COM>
2022-09-22 13:36:30 +0200

    smartphone

--- opleiding/leren programmeren/challenges/smartphone.py
+++ opleiding/leren programmeren/challenges/smartphone.py
@@ -1,8 +1,10 @@
 vraag1 = int(input("hoe duur is  de iphone ?"))
 vraag2 = int(input("hoe duur is  de galaxy ?"))
-
-verschilnd = vraag1 - vraag2
-if  verschilnd >= 51
-print = ("de iphone is het duurst, de telefoon kost:",vraag1 )
-print = ("de galaxy is goedkoopst,de telefoon kost:" ,vraag2)
-Print = ("het advies is dus de iphone te kopen .deze is namelijk" , verschilnd, "duurder dan de galaxy.")
+verschil = vraag1 - vraag2
+if verschil<50:
+    print("de iphone is het duurst, de telefoon kost:",vraag1 )
+    print("de galaxy is goedkoopst,de telefoon kost:" ,vraag2)
+    print("het advies is dus de iphone te kopen .deze is namelijk" ,verschil, "duurder dan de galaxy.")
+else:
+    print("de verschil is meer dan 50")
+    print("het advies is dus de galaxy te kopen .deze is namelijk goedkoper dan iphone")
