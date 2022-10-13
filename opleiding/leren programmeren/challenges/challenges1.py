6398a62d26f1b28458ed550264753030679d3b7f
AZIZ <azeznsasser2004@GMAIL.COM>
2022-10-13 10:12:27 +0200

    zwembad 3

---
+++ opleiding/leren programmeren/challenges/challenges1.py
@@ -0,0 +1,7 @@
+lang = int(8) 
+breed = int(3)
+diep = float(1.5)
+de_grootte = int(lang) * int(breed) * float(diep)
+afvoeren = float(32.5)
+afvoeren_prijs = float(de_grootte) * float(afvoeren)
+print(afvoeren_prijs)
---
+++ opleiding/leren programmeren/challenges/challenges2.py
@@ -0,0 +1,13 @@
+lang = int(8) 
+breed = int(3)
+diep = float(1.5)
+de_grootte = int(lang) * int(breed) * float(diep)
+afvoeren = float(32.5)
+afvoeren_prijs = float(de_grootte) * float(afvoeren)
+
+uitgraven = int(25)
+uitgraven_prijs = int(uitgraven) * float(de_grootte)
+totaal_prijs = float(afvoeren_prijs) + float(uitgraven_prijs)
+print("uitgraven:€",uitgraven_prijs) 
+print("afvoeren: €", afvoeren_prijs)
+print("totaal:€" , totaal_prijs) 
