d3705652203a1aefaae0f1dd46713b5b2be2b429
AZIZ <azeznsasser2004@GMAIL.COM>
2022-10-13 09:46:27 +0200

    zwembad

---
+++ opleiding/leren programmeren/challenges/challenges.py
@@ -0,0 +1,8 @@
+lang = int(8) 
+breed = int(3)
+diep = float(1.5)
+afvoeren = float(32.5)
+
+totaal_prijs = int(lang) * int(breed) * float(diep) * float(afvoeren)
+
+print(totaal_prijs)
--- opleiding/leren programmeren/module 02 omgaan met/deel 03/min_mix.py
+++ opleiding/leren programmeren/module 02 omgaan met/deel 03/min_mix.py
@@ -1,11 +1,12 @@
 a = int(input("geef een getal:"))
 b = int(input("geef een getal:"))
-max = a
-min = b
+
 if a > b :
+    max = a
     print (f"a is het grootste getal : {max}")
     
 elif a < b : 
+    min = a
     print(f"b is het grootste getal : {min}")
 
 else :
