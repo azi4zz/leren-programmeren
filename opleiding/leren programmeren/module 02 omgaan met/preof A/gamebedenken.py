d7db872c8827106e3a8681835c862389cac4fc55
AZIZ <azeznsasser2004@GMAIL.COM>
2022-12-01 13:00:36 +0100

    game flow

---
+++ opleiding/leren programmeren/module 02 omgaan met/preof A/gamebedenken.py
@@ -0,0 +1,4 @@
+# mijn game gaat over dat je bent in een bos en wilt uitgaan
+#  maar kan je niet want daar huisdieren en woeste dieren 
+# en je hebt altijd 2 uitgang en dan kan je kiezen vechten of tureg of rennen 
+# 
