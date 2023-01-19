e6937e4882a47ac5def6aa7a15c2a7196d146c7e
AZIZ <azeznsasser2004@GMAIL.COM>
2023-01-19 10:21:26 +0100

    getallen

---
+++ opleiding/leren programmeren/module 03 keer op keer/deel 1/de_tijd.py
@@ -0,0 +1,6 @@
+my_numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+for x in my_numbers:
+    if x -12 < 0 :
+        print(f'{x}:00 am')
+    else:
+        print(f'{x}:00 pm')    
